1a3abc942f3a1f89a67bfa91267b90a07ad86360
victormusembi11 <victor.musembi11@gmail.com>
2024-01-04 23:12:24 +0300

    integrated chakra ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import AppRouter from "./Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppRouter />
+    <ChakraProvider>
+      <AppRouter />
+    </ChakraProvider>
   </React.StrictMode>
 );
